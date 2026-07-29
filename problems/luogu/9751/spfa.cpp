60dedb6c517b0e4686b0d39ffe3ff90ade5623a3
rainboy <rainboylvx@qq.com>
2026-07-29 17:01:11 +0800

    luogu 9751: rewrite queue relaxation example

--- problems/luogu/9751/spfa.cpp
+++ problems/luogu/9751/spfa.cpp
@@ -1,116 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
-const ll MAXN = 10005;
-const ll MAXM = 20005;
-const ll INF = (1LL << 60);
-
-ll n, m, k;
-ll dis[MAXN][105];
-bool in_que[MAXN][105];
-
-struct Node {
-    ll u, mod;
+struct Edge {
+  int to;        // 终点
+  int open_time; // 这条边最早可以通过的时刻
 };
 
-struct LinkList {
-    struct Edge {
-        ll u, v, w, next;
-    };
+struct State {
+  int vertex;    // 当前所在的点
+  int remainder; // 当前时刻除以 k 的余数
+};
 
-    Edge e[MAXM];
-    ll h[MAXN], edge_cnt;
-
-    LinkList() {
-        edge_cnt = 0;
-        memset(h, -1, sizeof(h));
-    }
-
-    void add(ll u, ll v, ll w = 0) {
-        e[edge_cnt] = {u, v, w, h[u]};
-        h[u] = edge_cnt++;
-    }
-
-    Edge &operator[](ll i) {
-        return e[i];
-    }
-} graph;
-
-queue<Node> q;
-
-bool update(ll u, ll mod, ll val) {
-    if (dis[u][mod] > val) {
-        dis[u][mod] = val;
-        return true;
-    }
-    return false;
-}
-
-void init() {
-    cin >> n >> m >> k;
-    for (ll i = 1; i <= m; ++i) {
-        ll u, v, a;
-        cin >> u >> v >> a;
-        graph.add(u, v, a);
-    }
-
-    for (ll i = 0; i < MAXN; ++i) {
-        for (ll j = 0; j < 105; ++j) {
-            dis[i][j] = INF;
-        }
-    }
-    dis[1][0] = 0;
-}
-
-void spfa() {
-    q.push({1, 0});
-    in_que[1][0] = true;
-
-    while (!q.empty()) {
-        ll u = q.front().u;
-        ll mod = q.front().mod;
-        q.pop();
-        in_que[u][mod] = false;
-
-        ll d = dis[u][mod];
-
-        for (ll i = graph.h[u]; i != -1; i = graph[i].next) {
-            ll v = graph[i].v;
-            ll open_time = graph[i].w;
-
-            ll len = d;
-            if (len < open_time) {
-                ll cnt = (open_time - len + k - 1) / k;
-                len += cnt * k;
-            }
-
-            ++len;
-            ll v_mod = len % k;
-
-            if (update(v, v_mod, len)) {
-                if (!in_que[v][v_mod]) {
-                    in_que[v][v_mod] = true;
-                    q.push({v, v_mod});
-                }
-            }
-        }
-    }
-}
+const long long INF = (1LL << 60);
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    init();
-    spfa();
+  int n, m, k;
+  cin >> n >> m >> k;
 
-    if (dis[n][0] == INF) {
-        cout << -1 << '\n';
-    } else {
-        cout << dis[n][0] << '\n';
+  // g[u] 保存所有从 u 出发的有向边。
+  vector<vector<Edge>> g(n + 1);
+  for (int i = 0; i < m; ++i) {
+    int from, to, open_time;
+    cin >> from >> to >> open_time;
+    g[from].push_back({to, open_time});
+  }
+
+  // dist[u][r]：到达 u 且到达时刻 mod k = r 时，最早的真实到达时刻。
+  vector<vector<long long>> dist(n + 1, vector<long long>(k, INF));
+
+  // 普通队列只负责保存“距离变小、需要继续向外松弛”的状态。
+  // 同一个状态可能被多次更新，因此这不是“每个点只访问一次”的普通 BFS。
+  queue<State> q;
+  vector<vector<char>> in_queue(n + 1, vector<char>(k, false));
+
+  // 0 时刻乘车到达入口，所以初始状态是 (1, 0)。
+  dist[1][0] = 0;
+  q.push({1, 0});
+  in_queue[1][0] = true;
+
+  while (!q.empty()) {
+    State current = q.front();
+    q.pop();
+
+    int u = current.vertex;
+    int remainder = current.remainder;
+    in_queue[u][remainder] = false;
+
+    for (const Edge &edge : g[u]) {
+      long long use_edge_time = dist[u][remainder];
+
+      if (use_edge_time < edge.open_time) {
+        // 景区内不能原地等待。这里增加若干个 k，不表示在 u 等待，
+        // 而是把乘入口巴士的时刻整体推迟若干个 k。
+        // 整条已走路径的时刻会同时后移，余数不变，且之前的边仍然开放。
+        long long difference = edge.open_time - use_edge_time;
+        long long periods = (difference + k - 1) / k;
+        use_edge_time += periods * k;
+      }
+
+      // 每条道路恰好走 1 个单位时间。
+      long long next_time = use_edge_time + 1;
+      int next_remainder = next_time % k;
+
+      // 松弛状态 (edge.to, next_remainder)。
+      if (next_time >= dist[edge.to][next_remainder]) {
+        continue;
+      }
+
+      dist[edge.to][next_remainder] = next_time;
+
+      // 若该状态已经在队列中，不必重复加入；它出队时会读取最新的 dist。
+      if (!in_queue[edge.to][next_remainder]) {
+        q.push({edge.to, next_remainder});
+        in_queue[edge.to][next_remainder] = true;
+      }
     }
+  }
 
-    return 0;
+  // 离开景区的时刻必须是 k 的倍数，所以只接受余数为 0 的终点状态。
+  if (dist[n][0] == INF) {
+    cout << -1 << '\n';
+  } else {
+    cout << dist[n][0] << '\n';
+  }
+
+  return 0;
 }
