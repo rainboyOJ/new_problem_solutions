f76bc6ece54091297b6e651c90ee217eeac1f488
rainboy <rainboylvx@qq.com>
2026-07-31 19:41:51 +0800

    solve: add CSP201703A cake distribution

--- problems/shumeng/CSP201703A/brute.cpp
+++ problems/shumeng/CSP201703A/brute.cpp
@@ -3,15 +3,34 @@
  * rbook: -> https://rbook.roj.ac.cn  https://rbook2.roj.ac.cn
  * rainboy的学习导航网站: https://idx.roj.ac.cn
  * create_at: 2026-07-31 16:21
- * update_at: 2026-07-31 16:21
+ * update_at: 2026-07-31 19:40
  */
-// brute.cpp：小数据暴力解，用来帮助理解题意并辅助对拍。
+// brute.cpp：按题意逐个朋友、逐块蛋糕模拟分配过程。
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> cake(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> cake[i];
+    }
+
+    int position = 1;
+    int answer = 0;
+    while (position <= n) {
+        int current_weight = 0;
+        answer++;
+        while (position <= n && current_weight < k) {
+            current_weight += cake[position];
+            position++;
+        }
+    }
+    cout << answer << '\n';
 
     return 0;
 }
--- problems/shumeng/CSP201703A/main.cpp
+++ problems/shumeng/CSP201703A/main.cpp
@@ -3,7 +3,7 @@
  * rbook: -> https://rbook.roj.ac.cn  https://rbook2.roj.ac.cn
  * rainboy的学习导航网站: https://idx.roj.ac.cn
  * create_at: 2026-07-31 16:21
- * update_at: 2026-07-31 16:21
+ * update_at: 2026-07-31 19:40
  */
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,6 +11,26 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    int current_weight = 0;
+    int answer = 0;
+    for (int i = 1; i <= n; i++) {
+        int cake_weight;
+        cin >> cake_weight;
+        current_weight += cake_weight;
+        if (current_weight >= k) {
+            answer++;
+            current_weight = 0;
+        }
+    }
+    // 最后一个朋友即使没有拿到 k 重量，也会拿走全部剩余蛋糕。
+    if (current_weight > 0) {
+        answer++;
+    }
+    cout << answer << '\n';
 
     return 0;
 }
