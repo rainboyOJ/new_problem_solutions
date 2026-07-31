368bb57bc644106402069d0ae584f0b5c543ac8f
rainboy <rainboylvx@qq.com>
2026-07-31 20:34:07 +0800

    solve: add CSP202006A linear classifier

--- problems/shumeng/CSP202006A/brute.cpp
+++ problems/shumeng/CSP202006A/brute.cpp
@@ -3,15 +3,44 @@
  * rbook: -> https://rbook.roj.ac.cn  https://rbook2.roj.ac.cn
  * rainboy的学习导航网站: https://idx.roj.ac.cn
  * create_at: 2026-07-31 16:21
- * update_at: 2026-07-31 16:21
+ * update_at: 2026-07-31 20:32
  */
-// brute.cpp：小数据暴力解，用来帮助理解题意并辅助对拍。
+// brute.cpp：逐条查询、逐点检查两类点是否位于直线的两侧。
 #include <bits/stdc++.h>
 using namespace std;
+
+struct Point {
+    long long x, y;
+    char type;
+};
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int n, query_count;
+    cin >> n >> query_count;
+    vector<Point> point(n);
+    for (int i = 0; i < n; i++) cin >> point[i].x >> point[i].y >> point[i].type;
+    while (query_count--) {
+        long long theta0, theta1, theta2;
+        cin >> theta0 >> theta1 >> theta2;
+        int sign_a = 0, sign_b = 0;
+        for (int i = 0; i < n; i++) {
+            long long value = theta0 + theta1 * point[i].x + theta2 * point[i].y;
+            int sign = value > 0 ? 1 : -1;
+            if (point[i].type == 'A' && sign_a == 0) sign_a = sign;
+            if (point[i].type == 'B' && sign_b == 0) sign_b = sign;
+        }
+        bool correct = sign_a != sign_b;
+        for (int i = 0; i < n; i++) {
+            long long value = theta0 + theta1 * point[i].x + theta2 * point[i].y;
+            int sign = value > 0 ? 1 : -1;
+            if (point[i].type == 'A' && sign != sign_a) correct = false;
+            if (point[i].type == 'B' && sign != sign_b) correct = false;
+        }
+        cout << (correct ? "Yes" : "No") << '\n';
+    }
 
     return 0;
 }
--- problems/shumeng/CSP202006A/main.cpp
+++ problems/shumeng/CSP202006A/main.cpp
@@ -3,14 +3,46 @@
  * rbook: -> https://rbook.roj.ac.cn  https://rbook2.roj.ac.cn
  * rainboy的学习导航网站: https://idx.roj.ac.cn
  * create_at: 2026-07-31 16:21
- * update_at: 2026-07-31 16:21
+ * update_at: 2026-07-31 20:32
  */
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAXN = 1005;
+
+struct Point {
+    long long x, y;
+    char type;
+};
+
+int n, query_count;
+Point point[MAXN];
+
+int get_sign(long long value) {
+    return value > 0 ? 1 : -1;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    cin >> n >> query_count;
+    for (int i = 1; i <= n; i++) cin >> point[i].x >> point[i].y >> point[i].type;
+    while (query_count--) {
+        long long theta0, theta1, theta2;
+        cin >> theta0 >> theta1 >> theta2;
+        int sign_a = 0, sign_b = 0;
+        bool correct = true;
+        for (int i = 1; i <= n; i++) {
+            long long value = theta0 + theta1 * point[i].x + theta2 * point[i].y;
+            int sign = get_sign(value);
+            int &expected = point[i].type == 'A' ? sign_a : sign_b;
+            if (expected == 0) expected = sign;
+            else if (expected != sign) correct = false;
+        }
+        if (sign_a == sign_b) correct = false;
+        cout << (correct ? "Yes" : "No") << '\n';
+    }
 
     return 0;
 }
