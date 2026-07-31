4d0f8f156523d155dda543d776d3041800d8bbb4
rainboy <rainboylvx@qq.com>
2026-07-31 19:56:19 +0800

    solve: add CSP201809B grocery shopping

--- problems/shumeng/CSP201809B/brute.cpp
+++ problems/shumeng/CSP201809B/brute.cpp
@@ -3,15 +3,39 @@
  * rbook: -> https://rbook.roj.ac.cn  https://rbook2.roj.ac.cn
  * rainboy的学习导航网站: https://idx.roj.ac.cn
  * create_at: 2026-07-31 16:21
- * update_at: 2026-07-31 16:21
+ * update_at: 2026-07-31 19:55
  */
-// brute.cpp：小数据暴力解，用来帮助理解题意并辅助对拍。
+// brute.cpp：枚举两人的每一对装车区间，累加它们的交集长度。
 #include <bits/stdc++.h>
 using namespace std;
+
+struct Segment {
+    int left, right;
+};
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<Segment> first(n), second(n);
+    for (int i = 0; i < n; i++) {
+        cin >> first[i].left >> first[i].right;
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> second[i].left >> second[i].right;
+    }
+
+    int answer = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int overlap_left = max(first[i].left, second[j].left);
+            int overlap_right = min(first[i].right, second[j].right);
+            answer += max(0, overlap_right - overlap_left);
+        }
+    }
+    cout << answer << '\n';
 
     return 0;
 }
--- problems/shumeng/CSP201809B/main.cpp
+++ problems/shumeng/CSP201809B/main.cpp
@@ -3,14 +3,45 @@
  * rbook: -> https://rbook.roj.ac.cn  https://rbook2.roj.ac.cn
  * rainboy的学习导航网站: https://idx.roj.ac.cn
  * create_at: 2026-07-31 16:21
- * update_at: 2026-07-31 16:21
+ * update_at: 2026-07-31 19:55
  */
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Segment {
+    int left, right;
+};
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<Segment> first(n), second(n);
+    for (int i = 0; i < n; i++) {
+        cin >> first[i].left >> first[i].right;
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> second[i].left >> second[i].right;
+    }
+
+    int i = 0, j = 0;
+    int answer = 0;
+    while (i < n && j < n) {
+        int overlap_left = max(first[i].left, second[j].left);
+        int overlap_right = min(first[i].right, second[j].right);
+        if (overlap_left < overlap_right) {
+            answer += overlap_right - overlap_left;
+        }
+
+        if (first[i].right < second[j].right) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+    cout << answer << '\n';
 
     return 0;
 }
