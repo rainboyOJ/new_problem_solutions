35e39e74d156fcb334e4173c0ccec540ed8daf85
rainboy <rainboylvx@qq.com>
2026-08-04 12:25:34 +0800

    style: P1002/P1115 main.cpp 增加 Rainboy 头与教学注释

--- problems/luogu/1002/main.cpp
+++ problems/luogu/1002/main.cpp
@@ -1,20 +1,36 @@
+/**
+ * Author by Rainboy blog: https://rainboylv.com github: https://github.com/rainboylvx
+ * rbook: -> https://rbook.roj.ac.cn  https://rbook2.roj.ac.cn
+ * rainboy的学习导航网站: https://idx.roj.ac.cn
+ * create_at: 2026-08-04 12:24
+ * update_at: 2026-08-04 12:24
+ */
+
+/* P1002 [NOIP 2002 普及组] 过河卒 */
+/* 网格路径计数 DP：卒只能向右/向下走，马所在格与马一步能跳到的格子不能走。
+ * 每个格子的路径数 = 上方格子的路径数 + 左方格子的路径数。 */
+
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
 const int MAXN = 25;
 
-int n, m, hx, hy;
-ll dp[MAXN][MAXN];
-bool blocked[MAXN][MAXN];
+int n, m;             // 终点坐标 (n, m)，起点 (0, 0)
+int hx, hy;           // 马的位置
+ll dp[MAXN][MAXN];    // dp[x][y]：从 (0,0) 走到 (x,y) 的合法路径数
+bool blocked[MAXN][MAXN]; // blocked[x][y] = true 表示该格不能走
 
+// 马的控制格偏移：0 号是马自己，其余 8 个是马一步能跳到的位置
 int dx[] = {0, 1, 1, -1, -1, 2, 2, -2, -2};
 int dy[] = {0, 2, -2, 2, -2, 1, -1, 1, -1};
 
+// 判断 (x, y) 是否在棋盘内
 bool in_board(int x, int y) {
     return x >= 0 && x <= n && y >= 0 && y <= m;
 }
 
+// 把马所在格和 8 个控制格全部标记为不能走
 void mark_horse() {
     for (int i = 0; i < 9; i++) {
         int nx = hx + dx[i];
@@ -32,13 +48,15 @@ int main() {
     cin >> n >> m >> hx >> hy;
     mark_horse();
 
+    // 起点本身不能被马控制
     if (!blocked[0][0]) dp[0][0] = 1;
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j <= m; j++) {
             if (blocked[i][j]) {
-                dp[i][j] = 0;
+                dp[i][j] = 0;   // 不能走的格子路径数为 0
                 continue;
             }
+            // 卒只能从上方或左方走来，两个来源的路径数相加
             if (i > 0) dp[i][j] += dp[i - 1][j];
             if (j > 0) dp[i][j] += dp[i][j - 1];
         }
--- problems/luogu/1115/main.cpp
+++ problems/luogu/1115/main.cpp
@@ -1,11 +1,23 @@
+/**
+ * Author by Rainboy blog: https://rainboylv.com github: https://github.com/rainboylvx
+ * rbook: -> https://rbook.roj.ac.cn  https://rbook2.roj.ac.cn
+ * rainboy的学习导航网站: https://idx.roj.ac.cn
+ * create_at: 2026-08-04 12:24
+ * update_at: 2026-08-04 12:24
+ */
+
+/* P1115 最大子段和 */
+/* 一维线性 DP：以 i 结尾的最大子段和只有两种来源——
+ * 要么从 a[i] 重新开始，要么接到以 i-1 结尾的最优子段后面。 */
+
 #include <bits/stdc++.h>
 using namespace std;
 
 const int MAXN = 200005;
 
 int n;
-int a[MAXN];
-long long dp[MAXN]; // dp[i]：以 i 结尾的最大子段和
+int a[MAXN];             // 输入序列
+long long dp[MAXN];      // dp[i]：以第 i 个数结尾的最大子段和
 
 int main() {
     ios::sync_with_stdio(false);
@@ -16,9 +28,10 @@ int main() {
         cin >> a[i];
     }
 
-    dp[1] = a[1];
-    long long ans = dp[1];
+    dp[1] = a[1];              // 边界：第一个数只能自己成一段
+    long long ans = dp[1];     // 答案 = 所有结尾里的最大值
     for (int i = 2; i <= n; i++) {
+        // 转移：从 i 重新开始，或把 a[i] 接到前一个最优子段后面
         dp[i] = max(1LL * a[i], dp[i - 1] + a[i]);
         ans = max(ans, dp[i]);
     }
