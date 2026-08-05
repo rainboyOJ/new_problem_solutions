30c944f7e6035627da1f278ead85656bae1fe799
rainboy <rainboylvx@qq.com>
2026-08-05 11:30:25 +0800

    refactor: P1077 移除 pre 前缀和数组，改用直接三重循环枚举第 i 种花的盆数

--- problems/luogu/1077/main.cpp
+++ problems/luogu/1077/main.cpp
@@ -3,9 +3,9 @@
  * rbook: -> https://rbook.roj.ac.cn  https://rbook2.roj.ac.cn
  * rainboy的学习导航网站: https://idx.roj.ac.cn
  * create_at: 2026-08-05 10:40
- * update_at: 2026-08-05 10:40
+ * update_at: 2026-08-05 10:55
  */
-// 二维 DP + 前缀和优化：dp[i][j] 表示前 i 种花共摆 j 盆的方案数，不使用滚动数组。
+// 二维 DP：dp[i][j] 表示前 i 种花共摆 j 盆的方案数，直接枚举第 i 种花摆 k 盆。
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,9 +14,8 @@ const int MAXM = 105;
 const int MOD = 1000007;
 
 int n, m;
-int a[MAXN];                 // a[i] 表示第 i 种花最多能摆的盆数
-long long dp[MAXN][MAXM];    // dp[i][j] 前 i 种花共摆 j 盆的方案数
-long long pre[MAXM];         // 前缀和：pre[j] = dp[i-1][0..j] 的和
+int a[MAXN];                // a[i] 表示第 i 种花最多能摆的盆数
+long long dp[MAXN][MAXM];   // dp[i][j] 前 i 种花共摆 j 盆的方案数
 
 int main() {
     ios::sync_with_stdio(false);
@@ -28,20 +27,13 @@ int main() {
     // 边界：0 种花只能摆 0 盆，方案数为 1
     dp[0][0] = 1;
 
+    // 前 i 种花共摆 j 盆：枚举第 i 种花摆 k 盆 (0 <= k <= a[i])
     for (int i = 1; i <= n; i++) {
-        // 计算上一行 dp[i-1] 的前缀和
-        pre[0] = dp[i - 1][0];
-        for (int j = 1; j <= m; j++)
-            pre[j] = (pre[j - 1] + dp[i - 1][j]) % MOD;
-
-        // 转移：第 i 种花可以摆 0..a[i] 盆
         for (int j = 0; j <= m; j++) {
-            int l = max(0, j - a[i]);   // 第 i 种花最多贡献 a[i] 盆，所以 j 至少从 j-a[i] 取
-            long long val = pre[j];
-            if (l > 0) val -= pre[l - 1];
-            val %= MOD;
-            if (val < 0) val += MOD;
-            dp[i][j] = val;
+            for (int k = 0; k <= a[i] && k <= j; k++) {
+                dp[i][j] += dp[i - 1][j - k];
+                dp[i][j] %= MOD;
+            }
         }
     }
 
