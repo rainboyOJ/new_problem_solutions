e9cb7d59ca713741591b21d799c9acd6ce12db58
rainboy <rainboylvx@qq.com>
2026-09-17 10:20:40 +0800

    add graph2

--- public/relations2-graph/assets/index.js
+++ public/relations2-graph/assets/index.js
@@ -30,7 +30,7 @@ uniform vec4 uOutputTexture;
 vec4 filterVertexPosition( void )
 {
     vec2 position = aPosition * uOutputFrame.zw + uOutputFrame.xy;
-
+    
     position.x = position.x * (2.0 / uOutputTexture.x) - 1.0;
     position.y = position.y * (2.0*uOutputTexture.z / uOutputTexture.y) - uOutputTexture.z;
 
